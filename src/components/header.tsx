a9aeda9b61d36d84af772fedbf39586e5f0fd81f
ricarto1 <ricardotortolero@gmail.com>
2023-03-07 21:20:36 -0500

    lets go header components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,40 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Flex } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 export function Header() {
-    return (<header>
-      <Image src="/logo.svg" alt="" width={100} height={48} />
-      <nav>
-        <ul>
-          <li><Link href="">About Us</Link></li>
-          <li><Link href="">Woman</Link></li>
-          <li><Link href="">Men</Link></li>
-          <li><Link href="">Beauty</Link></li>
-          <li><Link href="">Accesories</Link></li>
-          <li><Link href="">Blog</Link></li>
-          <li><Link href="">Contact</Link></li>
-        </ul>
-      </nav>
-  
-      <div className='commerce-menu'>
-        <ul>
-          <li>
-            <Link href=""><Image src="/ico-search.svg" width="24" height="24" alt="" /></Link>
-          </li>
-          <li>
-            <Link href=""><Image src="/ico-globe.svg" width="24" height="24" alt="" /></Link>
-          </li>
-          <li>
-            <Link href=""><Image src="/ico-user.svg" width="24" height="24" alt="" /></Link>
-          </li>
-          <li>
-            <Link href=""><Image src="/ico-bag.svg" width="24" height="24" alt="" /></Link>
-          </li>
-        </ul>
-      </div>
-    </header>);
-  }
+    return (<Flex as="header" w="100%" justifyContent={"space-between"} alignItems="center" border="solid 2px" borderColor="gray.100">
+        <Box margin="16px 0">
+            <Image src="/logo.svg" alt="" width={100} height={48} />
+        </Box>
+        <nav>
+            <Flex as="ul" listStyleType="none" gap="32px">
+                <li><Link href="">About Us</Link></li>
+                <li><Link href="">Woman</Link></li>
+                <li><Link href="">Men</Link></li>
+                <li><Link href="">Beauty</Link></li>
+                <li><Link href="">Accesories</Link></li>
+                <li><Link href="">Blog</Link></li>
+                <li><Link href="">Contact</Link></li>
+            </Flex>
+        </nav>
+
+        <div className='commerce-menu'>
+            <Flex as="ul" listStyleType="none" gap="24px">
+                <li>
+                    <Link href=""><Image src="/ico-search.svg" width="24" height="24" alt="" /></Link>
+                </li>
+                <li>
+                    <Link href=""><Image src="/ico-globe.svg" width="24" height="24" alt="" /></Link>
+                </li>
+                <li>
+                    <Link href=""><Image src="/ico-user.svg" width="24" height="24" alt="" /></Link>
+                </li>
+                <li>
+                    <Link href=""><Image src="/ico-bag.svg" width="24" height="24" alt="" /></Link>
+                </li>
+            </Flex>
+        </div>
+    </Flex>);
+}
